cebd5fe6a314224bac8a97c772596ed76702fbbe
Dmitry Kuznetsov <d.kuznetsov@skyeng.ru>
2022-08-03 23:42:10 +0300

    [update] update redirect script

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,9 +1,6 @@
 (function(window, document) {
     const USER_LOCALE = 'userLocale';
     const savedUserLocale = window.localStorage.getItem(USER_LOCALE);
-    if (savedUserLocale) {
-        return;
-    }
 
     let lang = window.navigator.languages ? window.navigator.languages[0] : null;
     lang = lang || window.navigator.language || window.navigator.browserLanguage || window.navigator.userLanguage;
@@ -11,6 +8,13 @@
     do {
         shortLang = shortLang.split('-')[0];
     } while (shortLang.indexOf('-') !== -1);
+
+    if (savedUserLocale) {
+        if (shortLang !== savedUserLocale) {
+            window.localStorage.setItem(USER_LOCALE, shortLang);
+        }
+        return;
+    }
 
     window.localStorage.setItem(USER_LOCALE, shortLang);
     if (document.documentElement.lang === shortLang) {
