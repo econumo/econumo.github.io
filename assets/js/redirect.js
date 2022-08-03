aaf662920935cc26e9315b6af8fc6f5dd7a771b9
Dmitry Kuznetsov <d.kuznetsov@skyeng.ru>
2022-08-03 23:34:15 +0300

    [update] added english version

---
+++ assets/js/redirect.js
@@ -0,0 +1,38 @@
+(function(window, document) {
+    const USER_LOCALE = 'userLocale';
+    const savedUserLocale = window.localStorage.getItem(USER_LOCALE);
+    if (savedUserLocale) {
+        return;
+    }
+
+    let lang = window.navigator.languages ? window.navigator.languages[0] : null;
+    lang = lang || window.navigator.language || window.navigator.browserLanguage || window.navigator.userLanguage;
+    let shortLang = lang;
+    do {
+        shortLang = shortLang.split('-')[0];
+    } while (shortLang.indexOf('-') !== -1);
+
+    window.localStorage.setItem(USER_LOCALE, shortLang);
+    if (document.documentElement.lang === shortLang) {
+        return;
+    }
+
+    const availableLanguages = document.querySelectorAll('link[rel=alternate]');
+    let defaultUrl = null;
+    let redirectUrl = null;
+    availableLanguages.forEach((item) => {
+        if (!defaultUrl && item.getAttribute('hreflang') === 'x-default') {
+            defaultUrl = item.getAttribute('href');
+        }
+        if (!redirectUrl && item.getAttribute('hreflang') === shortLang) {
+            redirectUrl = item.getAttribute('href');
+        }
+    })
+
+    if (redirectUrl) {
+        window.location.replace(redirectUrl);
+    } else if (defaultUrl) {
+        window.location.replace(defaultUrl);
+    }
+})(window, document);
+
